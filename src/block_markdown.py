16a21ba1c5f4845cb0836c2a87a555097228e4b7
Fraegdegjevar <willhcode@gmail.com>
2025-04-21 17:12:42 +0100

    Added block_to_blocktype and tests

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,5 +1,49 @@
 from textnode import *
+import re
 
 def markdown_to_blocks(markdown):
     blocks = [block.strip() for block in markdown.split("\n\n") if block.strip()]
     return blocks
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph" 
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+    
+def block_to_blocktype(block):
+    # First 1-6 chrs are # followed by a space - heading
+    # re.match matches start of string only i.e ^ is implied in regex
+    if re.match(r"#{1,6} ", block) != None:
+        return BlockType.HEADING
+    if block[0:3] == "```" and block[-3:] == "```":
+        return BlockType.CODE
+    
+    #Now we need to consider if the block is an (un)ordered list or quote which requires line by line 
+    #inspection.  
+    def check_lines_start_with(lines, start_char):
+        for line in lines:
+            if line[0:len(start_char)] != start_char:
+                return False
+        return True
+    
+    lines = block.split("\n")
+
+    if check_lines_start_with(lines, ">"):
+        return BlockType.QUOTE
+    if check_lines_start_with(lines, "- "):
+        return BlockType.UNORDERED_LIST
+    
+    #if any line starts fail criteria for ordered list, return the only remaining type possible,
+    # paragraph.
+    expected_num = 0
+    for line in lines:
+        line_start =  re.match(r"^(\d+)(\. )", line)
+        if not line_start or int(line_start.group(1)) != expected_num + 1 or line_start.group(2) != ". ":
+            return BlockType.PARAGRAPH
+        expected_num += 1
+    return BlockType.ORDERED_LIST
+        
+    
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -33,4 +33,84 @@ This is the same paragraph on a new line
         ]
         self.assertListEqual(expected_blocks, blocks)
     
-                
+    def test_empty_string(self):
+        md = """
+        
+        """
+        self.assertListEqual(
+           [], markdown_to_blocks(md)
+           )
+        
+    def test_one_block(self):
+        md = "All text is in one block!"
+        expected_blocks = ["All text is in one block!"]
+        self.assertListEqual(expected_blocks, markdown_to_blocks(md))
+    
+class TestBlockToBlockType(unittest.TestCase):
+    def test_empty_string(self):
+        block = ""
+        self.assertEqual(BlockType.PARAGRAPH, block_to_blocktype(block))
+    
+    def test_basic_string(self):
+        block = "just text and nothing else"
+        self.assertEqual(BlockType.PARAGRAPH, block_to_blocktype(block))
+    
+    def test_heading(self):
+        block = "### this is a heading"
+        self.assertEqual(BlockType.HEADING, block_to_blocktype(block))
+ 
+    def test_wrong_heading_no_space(self):
+        block = "###this is a heading"
+        self.assertEqual(BlockType.PARAGRAPH, block_to_blocktype(block))
+     
+    def test_wrong_heading_hashes(self):
+        block = "####### this is a heading"
+        self.assertEqual(BlockType.PARAGRAPH, block_to_blocktype(block))    
+
+    def test_code(self):
+        block = "``` this is a heading```"
+        self.assertEqual(BlockType.CODE, block_to_blocktype(block))
+
+    def test_mismatched_code(self):
+        block = "``` this is a heading``"
+        self.assertEqual(BlockType.PARAGRAPH, block_to_blocktype(block))
+    
+    def test_quote(self):
+        block = ">this is a heading"
+        self.assertEqual(BlockType.QUOTE, block_to_blocktype(block))
+    
+    def test_ord_list(self):
+        block = """1. this
+2. is
+3. ordered"""
+        self.assertEqual(BlockType.ORDERED_LIST, block_to_blocktype(block))
+    
+    def test_wrong_number_ord_list(self):
+        block = """1. this
+4. isn't
+3. ordered!"""
+        self.assertEqual(BlockType.PARAGRAPH, block_to_blocktype(block))
+
+    def test_wrong_format_ord_list(self):
+        block = """1. this
+2. is
+3.ordered
+4. but missing a space after 3.!"""
+        self.assertEqual(BlockType.PARAGRAPH, block_to_blocktype(block))
+
+    def test_unord_list(self):
+        block = """- this
+- is
+- unordered!"""
+        self.assertEqual(BlockType.UNORDERED_LIST, block_to_blocktype(block))
+
+    def test_wrong_format_unord_list(self):
+        block = """- this
+-is
+- unordered!
+-but lacks spaces after -!"""
+        self.assertEqual(BlockType.PARAGRAPH, block_to_blocktype(block))
+
+    
+    
+    
