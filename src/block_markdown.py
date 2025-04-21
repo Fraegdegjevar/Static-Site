bf74ddae8f48dc36cf607410ebd3a0283167b1ba
Fraegdegjevar <willhcode@gmail.com>
2025-04-21 20:21:31 +0100

    added block_to_html_node, block_to_blocktype, block_text_to_children, wrap_child_html_nodes and tests.

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,4 +1,6 @@
 from textnode import *
+from convert_md_to_textnodes import *
+from htmlnode import *
 import re
 
 def markdown_to_blocks(markdown):
@@ -45,5 +47,61 @@ def block_to_blocktype(block):
             return BlockType.PARAGRAPH
         expected_num += 1
     return BlockType.ORDERED_LIST
+
+def block_text_to_children(block, ignore_markdown = False):
+    lines = block.split("\n")
+    child_nodes = []
+    
+    if ignore_markdown:
+        for line in lines:
+            child_node = TextNode(line, TextType.TEXT)
+            child_nodes.append(text_node_to_html_node(child_node))
+        return child_nodes
+    
+    for line in lines:
+        # convert line to text nodes then convert them all to leaf nodes
+        child_text_nodes = [text_node_to_html_node(leaf) for leaf in text_to_textnodes(line)] 
+        child_nodes.extend(child_text_nodes)
+    
+    return child_nodes
         
+def wrap_child_html_nodes(child_nodes, tag):
+    wrapped_child_nodes = []
+    for child in child_nodes:
+        wrapped_child_nodes.append(ParentNode(tag = tag, children = child))
+    return wrapped_child_nodes
+
+def block_to_html_node(block):
+    match (block_to_blocktype(block)):
+        case (BlockType.QUOTE):
+            block_text = "\n".join(line[1:] for line in block.splitlines())
+            return ParentNode(tag="blockquote", children = block_text_to_children(block_text))
+        case (BlockType.UNORDERED_LIST):
+            block_text = "\n".join(line[2:] for line in block.splitlines())
+            node_children = block_text_to_children(block_text)
+            return ParentNode(tag="ul", children = wrap_child_html_nodes(node_children, "li"))
+        case (BlockType.ORDERED_LIST):
+            block_text = "\n".join(line[3:] for line in block.splitlines())
+            node_children = block_text_to_children(block_text)
+            return ParentNode(tag="ol", children = wrap_child_html_nodes(node_children, "li"))
+        case (BlockType.CODE):
+            block_text = block[3:-3]
+            return ParentNode(tag="pre", children = ParentNode(tag="code", children = block_text_to_children(block_text, True)))
+        case (BlockType.PARAGRAPH):
+            return ParentNode(tag="p", children = block_text_to_children(block)) #no modification needed
+        case (BlockType.HEADING):
+            level = re.match(r"#{1,6}", block).group(0).count("#")
+            block_text = block[level:]
+            return ParentNode(tag=f"h{level}", children = block_text_to_children(block_text))
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    block_nodes = []
+    
+    for block in blocks:
+        block_nodes.append(block_to_html_node(block))
+    
+    return ParentNode("div", block_nodes)
+    
+          
     
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -111,6 +111,72 @@ class TestBlockToBlockType(unittest.TestCase):
 -but lacks spaces after -!"""
         self.assertEqual(BlockType.PARAGRAPH, block_to_blocktype(block))
 
+class TestBlockTextToChildren(unittest.TestCase):
+    def test_block_text_ignore_markdown(self):
+        text= "**ignore bold** text and this should _not_ be italic"
+        expected_nodes = [LeafNode(None, "**ignore bold** text and this should _not_ be italic")]
+        self.assertListEqual(expected_nodes, block_text_to_children(text, True))
     
+    def test_block_text_markdown(self):
+        text = "**bold text** text and this should _be italic_ and ooh an ![image text](https://www.google.co.uk) image"
+        expected_nodes = [
+            LeafNode("b", "bold text"),
+            LeafNode(None, " text and this should "),
+            LeafNode("i", "be italic"),
+            LeafNode(None, " and ooh an "),
+            LeafNode("img", "", props = {"src": "https://www.google.co.uk", "alt": "image text"}),
+            LeafNode(None, " image")
+            ]
+        self.assertListEqual(expected_nodes, block_text_to_children(text))
+        
+    def test_empty_block(self):
+        text = ""
+        expected_nodes = []
+        self.assertListEqual(expected_nodes, block_text_to_children(text))
     
+class TestWrapChildHtmlNodes(unittest.TestCase):
+    def test_wrap_nodes(self):
+        html_nodes = [
+            LeafNode("b", "Bold first item"),
+            LeafNode("i", "italic second item"),
+            LeafNode(None, "normal third item")
+            ]
+        expected_nodes = [
+            ParentNode("li", LeafNode("b", "Bold first item")),
+            ParentNode("li", LeafNode("i", "italic second item")),
+            ParentNode("li", LeafNode(None, "normal third item"))
+            ]
+        self.assertListEqual(expected_nodes, wrap_child_html_nodes(html_nodes, "li"))
+        
+
+class TestBlockToHtmlNode(unittest.TestCase):
+    def test_quote_block_with_md(self):
+        block = """>**quote1 bold** q1 not bold
+>_quote2 italic_ q2 not italic
+>`quote3 code` q3 not code"""
+        expected_children = [
+            LeafNode("b", "quote1 bold"),
+            LeafNode(None, " q1 not bold"),
+            LeafNode("i", "quote2 italic"),
+            LeafNode(None, " q2 not italic"),
+            LeafNode("code", "quote3 code"),
+            LeafNode(None, " q3 not code")
+        ]
+        expected_node = ParentNode(tag="blockquote", children = expected_children)
+        self.assertEqual(expected_node, block_to_html_node(block))
+    
+    def test_paragraph_with_md(self):
+        block = """This is **just** a basic paragraph."""
+        expected_children = [
+            LeafNode(None, "This is "),
+            LeafNode("b", "just"),
+            LeafNode(None, " a basic paragraph.")
+        ]
+        expected_node = ParentNode("p", expected_children)
+        self.assertEqual(expected_node, block_to_html_node(block))
+    
+    def test_ord_list_with_md(self):
+        
+    
+
     
