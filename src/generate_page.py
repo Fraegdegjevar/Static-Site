702e565fe765e5b3cd5bcf118c7d9e26abce83a1
Fraegdegjevar <willhcode@gmail.com>
2025-04-22 02:13:36 +0100

    added build.sh to deploy to docs for github sites. Amended generate_page and generate_pages_recursively to take a basepath from argv for testing locally/deploying to github site. Deleted public directory as we now need to write to docs.

--- src/generate_page.py
+++ src/generate_page.py
@@ -10,7 +10,7 @@ def extract_title(markdown):
             return line[2:].strip()
     raise Exception("No title header in markdown file!")
     
-def generate_page(from_path, template_path=None, dest_path=None):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as f:
         markdown = f.read()
@@ -24,6 +24,8 @@ def generate_page(from_path, template_path=None, dest_path=None):
     
     template_with_content = re.sub("{{ Content }}", html_content, template)
     template_with_title = re.sub("{{ Title }}", title, template_with_content)
+    template_with_title = template_with_title.replace('href="/', f'href="{basepath}')
+    template_with_title = template_with_title.replace('src="/', f'src="{basepath}')
     
     os.makedirs(os.path.dirname(dest_path), exist_ok = True)  
     
@@ -31,12 +33,12 @@ def generate_page(from_path, template_path=None, dest_path=None):
         f.write(template_with_title)
         f.close()
 
-def generate_pages_recursive(dir_path_content, template_path,  dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path,  dest_dir_path, basepath):
     for item in os.listdir(dir_path_content):
         item_path = os.path.join(dir_path_content, item)
         item_dest_path = os.path.join(dest_dir_path, item)
         if os.path.isfile(item_path):
-            generate_page(item_path,template_path, item_dest_path.replace("md", "html"))
+            generate_page(item_path,template_path, item_dest_path.replace("md", "html"), basepath)
         else:
             print(f"Entering directory: {item_path}")
-            generate_pages_recursive(item_path, template_path, item_dest_path)
+            generate_pages_recursive(item_path, template_path, item_dest_path, basepath)
--- src/main.py
+++ src/main.py
@@ -4,9 +4,16 @@ from convert_md_to_textnodes import *
 from block_markdown import *
 from generate_public import *
 from generate_page import *
+import sys
+
+if len(sys.argv) > 1:
+    basepath = sys.argv[1]
+else:
+    basepath = "/"
 
 def main():
-    copy_src_to_dest_dir("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
-
+    copy_src_to_dest_dir("static", "docs")
+    print(f"generating pages to basepath: {basepath}")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
+    
 main()
